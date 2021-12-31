d1e79f49fe26cc481baad7075255fdc47f37c53c
Spy Kab <ooo000spy000ooo@gmail.com>
2021-12-31 16:14:40 +0700

    restructure contract event

---
+++ src/main/java/land/metadefi/camel/MintNFTConverter.java
@@ -0,0 +1,2 @@
+package land.metadefi.camel;public class MintNFTConverter {
+}
---
+++ src/main/java/land/metadefi/model/NFT.java
@@ -0,0 +1,18 @@
+package land.metadefi.model;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigInteger;
+
+@Getter
+@Setter
+@RegisterForReflection
+public class MintNFT {
+    String type;
+    String symbol;
+    String address;
+    String txnHash;
+    BigInteger tokenId;
+}
